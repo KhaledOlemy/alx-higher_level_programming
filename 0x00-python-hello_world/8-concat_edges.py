01afa72d229b77d81fe7863a118012edf75e3338
khaledolemy <khaledolemy@gmail.com>
2023-10-30 18:06:47 +0200

    create new string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f"{str[str.find('object'):str.find('language')-1} {str[str.find('with'):str.find('very')-1]} {str[:str.find(' ')}"
+str = f"{str[39:67]}{str[107:112]}{str[:6]}"
 print(str)
