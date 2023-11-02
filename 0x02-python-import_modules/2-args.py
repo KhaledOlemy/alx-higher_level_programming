bb3370c7f62b5ac707a7be7972e1a0668a9ab28c
khaledolemy <khaledolemy@gmail.com>
2023-11-02 23:38:37 +0200

    argv in python too

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     if len(argv) == 0:
         print("0 arguments.")
     else:
-        print("{} arguments.".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for i in range(len(argv)-1):
         print("{}: {}".format(i + 1, argv[i+1]))
