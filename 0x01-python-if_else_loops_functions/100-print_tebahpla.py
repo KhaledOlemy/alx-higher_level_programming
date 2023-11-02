ef195d9051a669928515b0543b604d9f6c06f719
khaledolemy <khaledolemy@gmail.com>
2023-11-02 02:23:33 +0200

    AlPhAbEt

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+char = 91
+minus = -33
+plus = 31
+while char != 65:
+    x = chr(char)
+    if char >= 97:
+        char += minus
+    else:
+        char += plus
+    print(chr(char), end="")
