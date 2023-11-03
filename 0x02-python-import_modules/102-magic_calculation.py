ca7f338f0d56457545edfecdda3175bf775c237d
khaledolemy <khaledolemy@gmail.com>
2023-11-03 04:51:25 +0200

    READ BYTES

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """magic_calculation"""
 def magic_calculation(a, b):
+    """magic_calculation"""
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
