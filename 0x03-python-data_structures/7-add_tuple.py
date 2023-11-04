bf0e73d61aaceab2430af196a12eb22d4f0f8f4f
khaledolemy <khaledolemy@gmail.com>
2023-11-04 07:51:49 +0200

    tuples work

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,9 +6,7 @@ def if_not_ret_zero(tuple_i=(), idx=0):
         out = 0
     return out
 
-
 def add_tuple(tuple_a=(), tuple_b=()):
     num_1 = if_not_ret_zero(tuple_a, 0) + if_not_ret_zero(tuple_b, 0)
     num_2 = if_not_ret_zero(tuple_a, 1) + if_not_ret_zero(tuple_b, 1)
     return (num_1, num_2)
-
