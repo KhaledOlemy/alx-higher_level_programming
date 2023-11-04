0b450973d17c6e4414a4d5456a4014a3faa97e84
khaledolemy <khaledolemy@gmail.com>
2023-11-04 07:29:28 +0200

    No Cs

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    out = []
+    if not my_string:
+        return None
+    for letter in my_string:
+        if letter != "c" and letter != "C":
+            out.append(letter)
+    return "".join(out)
