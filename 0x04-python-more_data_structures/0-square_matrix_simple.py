c34b82878c39961bf2cade26588053ca2514ec83
khaledolemy <khaledolemy@gmail.com>
2023-11-19 19:20:44 +0200

    0th square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/pyton3
 def square_matrix_simple(matrix=[]):
-    return [[j**2 for j in matrix[i]] for i in range(len(matrix))]
+    mat = [[j**2 for j in matrix[i]] for i in range(len(matrix))]
+    return mat
