34a51f6d724d281221597bdcf9aa7270952fdb4a
khaledolemy <khaledolemy@gmail.com>
2023-11-20 15:52:54 +0200

    she is 10 but out of 10000

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    maxi = max(list(a_dictionary.values()))
+    maxi = [i for i in list(a_dictionary.values()) if i]
+    if not maxi:
+        return None
+    maxi = max(maxi)
     top = [name for name in a_dictionary if a_dictionary[name] == maxi]
     return top[0]
