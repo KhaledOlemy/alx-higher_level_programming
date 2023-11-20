69cbd497c75165cc7863547125c007ffa18696b5
khaledolemy <khaledolemy@gmail.com>
2023-11-20 16:51:57 +0200

    Roman's old

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    try:
+        roman_string.strip()
+    except:
+        return 0
+    if not roman_string:
+        return 0
     convert = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     string = '|'.join(roman_string).split('|')
     out = 0
