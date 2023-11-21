604a63162bd3efac987693cc64aa3f1fd7fdf8c1
khaledolemy <khaledolemy@gmail.com>
2023-11-22 00:02:33 +0200

    I hate magic

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,16 +6,19 @@ import math
 class MagicClass:
     """MagicClass for a circle"""
     def __init__(self, radius=0):
-        """Initializes a MagicClass instance"""
+        """Initializes a MagicClass instance
+        Args:
+            radius: radius of MagicClass instance circle.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Disassembly of area:"""
+        """Return the area of the instance of MagicClass circle"""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Disassembly of circumference:"""
+        """Return the circumference of the instance of MagicClass circle"""
         return (2 * math.pi * self.__radius)
