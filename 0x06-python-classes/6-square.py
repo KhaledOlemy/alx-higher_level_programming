3dc2aa94ecb757111e71aaacaaee0c70680a7e6f
khaledolemy <khaledolemy@gmail.com>
2023-11-21 21:28:07 +0200

    position is key

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Square class definition"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        print(self.__position[1]*"\n", end="")
+        if self.__size == 0:
+            print(self.__position[0]*" ", end="")
+            print("")
+        else:
+            for i in range(self.__size):
+                print(self.__position[0]*" ", end="")
+                print("#"*self.__size)
