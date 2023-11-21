425aa44a9657fb4723c0ad8a6982912760f77139
khaledolemy <khaledolemy@gmail.com>
2023-11-21 21:35:31 +0200

    position is key neg-handle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple):
+        if not isinstance(value, tuple) or any(i <= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
