ca329f588a9a4870f601b42b13fe5a78aa1e48d2
khaledolemy <khaledolemy@gmail.com>
2023-11-29 12:34:06 +0200

    POWER is POWER

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """ This class Rectangle represents a rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
