c3e48f5209ef9e1ea0437fd98801eb2dcb5cf7ff
khaledolemy <khaledolemy@gmail.com>
2023-11-29 11:58:54 +0200

    no __ in init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """ This class Rectangle represents a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
