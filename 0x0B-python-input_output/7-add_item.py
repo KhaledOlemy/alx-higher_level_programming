6cac2897adde454fa34ed2728b0622841b061352
khaledolemy <khaledolemy@gmail.com>
2023-12-08 18:29:49 +0200

    7th is great

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" ADDING ITEMS TO LIST """
+import sys
+
+
+if __name__ == "__main__":
+    """ add item to list """
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    try:
+        list_items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        list_items = []
+    list_items.extend(sys.argv[1:])
+    save_to_json_file(list_items, "add_item.json")
