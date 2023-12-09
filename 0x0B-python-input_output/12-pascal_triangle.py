8a1e70b8f64e275e5393c9148283b61ead2cdb6d
khaledolemy <khaledolemy@gmail.com>
2023-12-09 14:16:07 +0200

    Pascal innvo

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """ pascal triangle """
+    pass
+    prev = None
+    matrix = []
+    for i in range(1, n+1):
+        out = [1] * i
+        if i == 1 or i == 2:
+            prev = out
+            matrix.append(out)
+            continue
+        for j in range(1, len(out) - 1):
+            out[j] = prev[j - 1] + prev[j]
+        prev = out
+        matrix.append(out)
+        continue
+    return matrix
