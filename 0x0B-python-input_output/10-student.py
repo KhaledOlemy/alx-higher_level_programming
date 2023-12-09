0931ec89ea8c27f7669fddff52ff6cc0464a6e3c
khaledolemy <khaledolemy@gmail.com>
2023-12-09 13:23:07 +0200

    10th is sleek

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,7 +14,7 @@ class Student:
     def to_json(self, attrs=None):
         """ Student to json of certain attributes"""
         out_dict = dict(self.__dict__)
-        if attrs:
+        if attrs is not None:
             attr_not_in = [i for i in list(out_dict.keys()) if i not in attrs]
             for attr in attr_not_in:
                 del out_dict[attr]
