5d8a08f4da375fdf59278920ffc6551fb2aa7aa2
khaledolemy <khaledolemy@gmail.com>
2023-12-12 03:04:52 +0200

    rectangle fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ rectangle initiation """
-
         self.__width = width
         self.__height = height
         self.__x = x
@@ -18,47 +17,39 @@ class Rectangle(Base):
     @property
     def width(self):
         """ width property """
-
         return self.__width
 
     @width.setter
     def width(self, width):
         """ width setter """
-
         self.__width = width
 
     @property
     def height(self):
         """ height property """
-
         return self.__height
 
     @height.setter
-    def width(self, height):
+    def height(self, height):
         """ height setter """
-
         self.__height = height
 
     @property
     def x(self):
         """ x property """
-
         return self.__x
 
     @x.setter
     def x(self, x):
         """ x setter """
-
         self.__x = x
 
     @property
     def y(self):
         """ y property """
-
         return self.__y
 
     @y.setter
     def y(self, y):
         """ y setter """
-
         self.__y = y
