2dfc4fb4e74ebae8dde9be54935f309d7579b7cb
khaledolemy <khaledolemy@gmail.com>
2023-12-12 04:22:23 +0200

    back to rectangle v2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,10 @@ class Rectangle(Base):
                     self.__x = val
                 elif key == "y":
                     self.__y = val
+
+    def to_dictionary(self):
+        return {"id": self.id,
+                "width": self.__width,
+                "height": self.__height,
+                "x": self.__x,
+                "y": self.__y}
