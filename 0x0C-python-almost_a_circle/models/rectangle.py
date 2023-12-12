f4af1879e913d93e36d05de6ba902f9149490f3b
khaledolemy <khaledolemy@gmail.com>
2023-12-12 03:14:09 +0200

    rectangle v2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,9 +8,13 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ rectangle initiation """
+        self.checker("width", width)
         self.__width = width
+        self.checker("height", height)
         self.__height = height
+        self.checker("x", x)
         self.__x = x
+        self.checker("y", y)
         self.__y = y
         super().__init__(id)
 
@@ -22,6 +26,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """ width setter """
+        self.checker("width", width)
         self.__width = width
 
     @property
@@ -32,6 +37,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """ height setter """
+        self.checker("height", height)
         self.__height = height
 
     @property
@@ -42,6 +48,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """ x setter """
+        self.checker("x", x)
         self.__x = x
 
     @property
@@ -52,4 +59,16 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """ y setter """
+        self.checker("y", y)
         self.__y = y
+
+    def checker(self, varname, value):
+        """ Checker for errors """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(varname))
+        if varname in ["width", "height"]:
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(varname))
+        if varname in ["x", "y"]:
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(varname))
