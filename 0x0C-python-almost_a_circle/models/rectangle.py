3cae4e0c2f937f976e5b5414fe1d0f9be9ec599e
khaledolemy <khaledolemy@gmail.com>
2023-12-12 02:56:52 +0200

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" RECTANGLE FILE """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class Rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ rectangle initiation """
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ width property """
+
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ width setter """
+
+        self.__width = width
+
+    @property
+    def height(self):
+        """ height property """
+
+        return self.__height
+
+    @height.setter
+    def width(self, height):
+        """ height setter """
+
+        self.__height = height
+
+    @property
+    def x(self):
+        """ x property """
+
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+
+        self.__x = x
+
+    @property
+    def y(self):
+        """ y property """
+
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ y setter """
+
+        self.__y = y
