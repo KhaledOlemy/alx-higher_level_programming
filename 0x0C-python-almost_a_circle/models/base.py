9686cc2196d9bcdc3dae0447de2bce1cf5148391
khaledolemy <khaledolemy@gmail.com>
2023-12-12 04:36:46 +0200

    base loads

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,12 +18,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ save to json string """
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ save to a file """
         file = cls.__name__ + ".json"
         with open(file, "w") as f:
             if not list_objs:
@@ -33,3 +35,8 @@ class Base:
                 for i in list_objs:
                     dicts.append(i.to_dictionary())
                 f.write(cls.to_json_string(dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ json loads """
+        return json.loads(json_string)
