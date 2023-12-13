a807f80cdb53d1a18ba7ff281c6968942d2e647b
khaledolemy <khaledolemy@gmail.com>
2023-12-13 08:56:47 +0200

    area exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" BASE GEOMETRY """
+
+
+class BaseGeometry:
+    """ BASE CLASS """
+    def area(self):
+        """ area func """
+        raise Exception("area() is not implemented")
