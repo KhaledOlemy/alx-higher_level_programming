cf6b3c827d5ae511917199870de877dcc3cb66ca
khaledolemy <khaledolemy@gmail.com>
2023-12-13 09:55:37 +0200

    1-my_list fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,11 @@
 class MyList(list):
     """ MyList class """
 
+    def __init__(self):
+        """ mylist init """
+        super().__init__()
+
     def print_sorted(self):
         """ print sorted list """
-        print(sorted(self))
+        newlist = sorted(self)
+        print(newlist)
