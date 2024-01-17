70aa6c247034d579b8350368c9c02d1d33bf5d9a
khaledolemy <khaledolemy@gmail.com>
2024-01-17 20:17:14 +0200

    genred

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
-SELECT sh.title, gn.genre_id FROM tv_shows sh FULL OUTER JOIN tv_show_genres gn ON (sh.id = gn.show_id) ORDER BY sh.title ASC, gn.genre_id ASC;
+SELECT sh.title, gn.genre_id FROM tv_shows sh LEFT JOIN tv_show_genres gn ON (sh.id = gn.show_id) ORDER BY sh.title ASC, gn.genre_id ASC;
