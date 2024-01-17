ecc082e904ef13f00d28d1ede7889fb44e516dc1
khaledolemy <khaledolemy@gmail.com>
2024-01-17 21:20:54 +0200

    100s

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,7 +2,8 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
-SELECT tv_gn.name from tv_genres tv_gn
+
+SELECT tv_gn.name FROM tv_genres tv_gn
 LEFT JOIN tv_show_genres tv_sh_gn ON (tv_sh_gn.genre_id = tv_gn.id)
 LEFT JOIN tv_shows tv_sh ON (tv_sh_gn.show_id = tv_sh.id)
 WHERE tv_sh.title <> 'Dexter'
