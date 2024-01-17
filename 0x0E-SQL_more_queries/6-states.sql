bda0e9b4942b9090ee8a3abbb0e0301c04fd0ed7
khaledolemy <khaledolemy@gmail.com>
2024-01-17 18:47:21 +0200

    states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,5 @@
 -- hbtn_0d_usa) on your MySQL server.
 -- states: id INT PRIMARY KEY, auto generated, name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
