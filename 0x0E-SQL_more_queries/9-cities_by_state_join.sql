d59596f4b5c7e098b6c8393133901d4070089fcb
khaledolemy <khaledolemy@gmail.com>
2024-01-17 19:15:02 +0200

    joined

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- Each record should display: cities.id - cities.name - states.name
 -- sorted in ascending order by cities.id/one SELECT statement
-SELECT c.id, c.name, s.name FROM cities c NATURAL JOIN states s ON (c.state_id = s.id) ORDER BY c.id ASC;
+SELECT c.id, c.name, s.name FROM cities c LEFT JOIN states s ON (c.state_id = s.id) ORDER BY c.id ASC;
