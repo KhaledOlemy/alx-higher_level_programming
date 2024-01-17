c9ee2f331ef80ee5def76cc24a79bee5419d8e4c
khaledolemy <khaledolemy@gmail.com>
2024-01-17 20:58:15 +0200

    14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- lists all genres of the show Dexter.
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+SELECT tv_gn.name FROM tv_genres tv_gn INNER JOIN tv_show_genres tv_sh_gn ON (tv_sh_gn.genre_id=tv_gn.id) WHERE (tv_sh.name='Dexter') ORDER BY tv_gn.name;
