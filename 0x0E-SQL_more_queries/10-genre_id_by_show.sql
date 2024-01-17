fe946cebd888535171073ffd83b151aab66cb59d
khaledolemy <khaledolemy@gmail.com>
2024-01-17 20:04:09 +0200

    joined

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- one SELECT statement
-SELECT sh.title, gn.genre_id FROM tv_shows sh LEFT JOIN tv_show_genres gn ON (sh.id = gn.show_id) ORDER BY sh.title ASC, gn.genre_id ASC;
+SELECT sh.title, gn.genre_id FROM tv_shows sh LEFT INNER JOIN tv_show_genres gn ON (sh.id = gn.show_id) ORDER BY sh.title ASC, gn.genre_id ASC;
