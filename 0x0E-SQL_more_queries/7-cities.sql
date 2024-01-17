9d880244d50cde7f1d00ec353d29418a3458a2ef
khaledolemy <khaledolemy@gmail.com>
2024-01-17 18:52:19 +0200

    cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database \
+-- hbtn_0d_usa) on your MySQL server.
+-- cities: id INT primary key, auto generated.
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to\
+-- id of the states table, name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL FOREIGN KEY REFERENCES states.id, name VARCHAR(256) NOT NULL);
