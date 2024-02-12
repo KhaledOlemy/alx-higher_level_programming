4c2f2605b9319f36be542d5fe81261ae9daafa77
khaledolemy <khaledolemy@gmail.com>
2024-02-13 01:47:04 +0200

    looped

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const myVar = parseInt(process.argv[2]);
+let i;
+if (isNaN(myVar)) {
+  console.log('Missing number of occurences');
+} else {
+  for (i = 0; i < myVar; i++) {
+    console.log('C is fun');
+  }
+}
