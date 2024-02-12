48db846db5d32864fc5f74bc74a143cb21a4e36b
khaledolemy <khaledolemy@gmail.com>
2024-02-13 01:38:38 +0200

    loop_n_list

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const list1 = ['C', 'Python', 'JavaScript'];
+const list2 = ['fun', 'cool', 'amazing'];
+let i;
+for (i = 0; i < list1.length; i++) {
+  console.log(list1[i], 'is', list2[i]);
+}
