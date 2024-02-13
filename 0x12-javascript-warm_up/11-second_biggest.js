bf2e54f79bed0da990c2aedfd160c501d32a2f52
khaledolemy <khaledolemy@gmail.com>
2024-02-13 02:27:11 +0200

    second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+let i;
+let max = -Infinity;
+let secmax = -Infinity;
+const lst = process.argv;
+const len = lst.length;
+
+if (len === 2 || len === 3) {
+  console.log(0);
+} else {
+  for (i = 2; i < len + 2; i++) {
+    if (parseInt(lst[i]) > secmax) {
+      if (parseInt(lst[i]) > max) {
+        secmax = max;
+        max = parseInt(lst[i]);
+      } else {
+        secmax = parseInt(lst[i]);
+      }
+    }
+  }
+}
+console.log(secmax);
