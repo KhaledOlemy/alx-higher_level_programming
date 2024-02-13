27593af8728716b1372472ffd9a7e9f9a7bcd34e
khaledolemy <khaledolemy@gmail.com>
2024-02-13 02:42:39 +0200

    call_me_daddy

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 let i;
 exports.callMeMoby = function (x, theFunction) {
-    for (i = 0; i < x; i++)
-    {
-	theFunction();
-    }
+  for (i = 0; i < x; i++) {
+    theFunction();
+  }
 };
