df7dc4dae682fbeddf4523ed6c36376c88c10ec5
khaledolemy <khaledolemy@gmail.com>
2024-02-13 02:06:46 +0200

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+let Out;
+
+function factorial (input) {
+  if (input === 1) {
+    return input;
+  } else {
+    return input * factorial(input - 1);
+  }
+}
+if (isNaN(process.argv[2])) {
+  console.log(1);
+} else {
+  Out = factorial(parseInt(process.argv[2]));
+  console.log(Out);
+}
