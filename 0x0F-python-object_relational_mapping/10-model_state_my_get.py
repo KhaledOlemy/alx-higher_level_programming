ce642cc438465666fbb2e8afbe3ce2f9050f1737
khaledolemy <khaledolemy@gmail.com>
2024-02-21 08:05:15 +0200

    state_id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+x
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    mainSession = sessionmaker(bind=engine)
+    session = mainSession()
+    ins = session.query(State).filter(State.name == sys.argv[4])
+    if ins:
+        print(ins[0].id)
+    else:
+        print("Not found")
