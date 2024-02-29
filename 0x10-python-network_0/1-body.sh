6b53fa42dc5f8a478bcfeb9531d23d674917d338
khaledolemy <khaledolemy@gmail.com>
2024-02-29 22:02:54 +0200

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GET only the body of a 200 page
-code=$(curl -sI $1 | grep -i HTTP | awk 'NR==1{print $2}')
+code=$(curl -sIL $1 | grep -i HTTP | awk 'NR==1{print $2}')
 if [ $code -eq 200 ]; then
-    curl -s $1;
+    curl -sL $1;
 fi
