a8e51872f050fa37ebd31cfa01a5f84e1b016850
khaledolemy <khaledolemy@gmail.com>
2024-03-01 00:38:57 +0200

    100_fix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get status code only
-curl https://www.google.com --silent --output /dev/null --write-out "%{http_code}"
+curl $1 --silent --output /dev/null --write-out "%{http_code}"
