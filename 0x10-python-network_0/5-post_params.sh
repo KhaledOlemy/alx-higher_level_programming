d1c47fae52af64578a306cad2d60680dd43d900d
khaledolemy <khaledolemy@gmail.com>
2024-02-29 23:04:10 +0200

    POST_request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request
-curl -X POST -d "email=test%40gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" $1
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
