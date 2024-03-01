bf3359ac9fb724df54c6731e1839f4c5b5359670
khaledolemy <khaledolemy@gmail.com>
2024-03-01 22:06:29 +0200

    refix_main_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,8 @@ get body content and type
 """
 import requests
 
-body = requests.get('https://alx-intranet.hbtn.io/status').text
-print('Body response:')
-print('\t- type: {}'.format(type(body)))
-print('\t- content: {}'.format(body))
+if __name__ == "__main__":
+    body = requests.get('https://alx-intranet.hbtn.io/status').text
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
