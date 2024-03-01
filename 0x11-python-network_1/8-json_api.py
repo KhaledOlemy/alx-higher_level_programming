69ab74e14a22ebf5975a36208b3115d8df911ce8
khaledolemy <khaledolemy@gmail.com>
2024-03-01 23:31:23 +0200

    JSON_API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+search API and find queries
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+        url = "http://0.0.0.0:5000/search_user"
+        data = f"q={q}"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+        resp = requests.post(url, data=data, headers=headers)
+        if resp.json():
+            print(f"[{resp.json().get('id')}] {resp.json().get('name')}")
+        else:
+            print("No result")
