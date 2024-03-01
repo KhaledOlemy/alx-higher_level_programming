2f2f7875aff0cc243cfa23805b26db9c861d4296
khaledolemy <khaledolemy@gmail.com>
2024-03-01 22:54:10 +0200

    errors_requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+get url response, handle if status is not 200
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    resp = requests.get(sys.argv[1], allow_redirects=False)
+    if resp.status_code == 200:
+        print(resp.text)
+    else:
+        print(resp.status_code)
