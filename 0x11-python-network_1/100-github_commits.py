b8fd5992726ba4961b10c0b68865866291d30dc9
khaledolemy <khaledolemy@gmail.com>
2024-03-02 00:28:12 +0200

    10_commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
+get 10 commits of input user and repo passed as args
 """
+
 import requests
 import sys
-
-url = "https://api.github.com/repos/{}/{}/commits?per_page=10&page=1"
-url = url.format(sys.argv[2], sys.argv[1])
-try:
-    resp = requests.get(url).json()
-    for i in resp:
-        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
-except ValueError:
-    print("Invalid Json")
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10&page=1"
+    url = url.format(sys.argv[2], sys.argv[1])
+    try:
+        resp = requests.get(url).json()
+        for i in resp:
+            print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+    except ValueError:
+        print("Invalid Json")
