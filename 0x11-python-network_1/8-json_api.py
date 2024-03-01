827d8a36d08914f2a6960375fc9eca643ddc01c3
khaledolemy <khaledolemy@gmail.com>
2024-03-01 23:36:23 +0200

    fix_empty_case

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         q = ""
     else:
         q = sys.argv[1]
-        url = "http://0.0.0.0:5000/search_user"
-        data = f"q={q}"
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-        }
-        resp = requests.post(url, data=data, headers=headers)
-        if resp.json():
-            print(f"[{resp.json().get('id')}] {resp.json().get('name')}")
-        else:
-            print("No result")
+    url = "http://0.0.0.0:5000/search_user"
+    data = f"q={q}"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    }
+    resp = requests.post(url, data=data, headers=headers)
+    if resp.json():
+        print(f"[{resp.json().get('id')}] {resp.json().get('name')}")
+    else:
+        print("No result")
