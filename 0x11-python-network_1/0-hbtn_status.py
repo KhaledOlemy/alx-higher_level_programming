bbc88df230034f3b2fec8e9b10fb00fb0e6a3284
khaledolemy <khaledolemy@gmail.com>
2024-03-01 21:11:17 +0200

    fix_not_imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+get the content of the website:
+'https://alx-intranet.hbtn.io/status'
+"""
 from urllib import request
-with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-    resp = resp.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(resp)))
-    print('\t- content: {}'.format(resp))
-    print('\t- utf8 content: {}'.format(resp.decode('UTF8')))
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        resp = resp.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(resp)))
+        print('\t- content: {}'.format(resp))
+        print('\t- utf8 content: {}'.format(resp.decode('UTF8')))
