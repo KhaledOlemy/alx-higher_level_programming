809c2c1038b8a79d633330a607592be629ccfd9b
khaledolemy <khaledolemy@gmail.com>
2024-03-27 00:29:23 +0200

    request_first_req

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+console.log(process.argv[2]);
+process.stdout.write('code: ');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`${response.statusCode}`);
+});
