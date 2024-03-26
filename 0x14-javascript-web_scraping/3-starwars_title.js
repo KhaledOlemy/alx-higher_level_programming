6348edbe571f458297d81f401a8ebd347bfb3503
khaledolemy <khaledolemy@gmail.com>
2024-03-27 01:59:33 +0200

    starWars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const movieNum = process.argv[2];
+const request = require('request');
+const reqUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieNum;
+request(reqUrl, function (n0, n1, body) {
+  console.log(JSON.parse(body).title);
+});
