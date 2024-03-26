a44f8561cace3e6fe19a6f493cd996d4929aab71
khaledolemy <khaledolemy@gmail.com>
2024-03-27 00:51:22 +0200

    combine_stdout

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 console.log(process.argv[2]);
-process.stdout.write('code: ');
 request.get(process.argv[2]).on('response', function (response) {
-  console.log(`${response.statusCode}`);
+  console.log(`code: ${response.statusCode}`);
 });
