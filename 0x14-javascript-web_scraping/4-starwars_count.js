a562ec8f37c017792576f92c7415119e53b736ce
khaledolemy <khaledolemy@gmail.com>
2024-03-27 02:04:16 +0200

    request_and_save_content_to_file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const reqUrl = process.argv[2];
+const fName = process.argv[3];
+console.log(reqUrl);
+console.log(fName);
+request(reqUrl, function (n0, n1, body) {
+    fs.writeFileSync(fName, body, { encoding: 'utf-8' });
+});
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const reqUrl = process.argv[2];
+const fName = process.argv[3];
+request(reqUrl, function (n0, n1, body) {
+  fs.writeFileSync(fName, body, { encoding: 'utf-8' });
+});
