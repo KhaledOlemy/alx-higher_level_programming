59ab9bf3ae42d6dfedff3756cf1d8b9bfb1eabfe
khaledolemy <khaledolemy@gmail.com>
2024-03-27 02:31:41 +0200

    starwars_count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const reqUrl = 'https://swapi-api.alx-tools.com/api/people/18';
+const reqUrl = process.argv[2];
+let count = 0;
 request(reqUrl, function (n0, n1, body) {
-  console.log(JSON.parse(body).films.length);
+  let moviesList = JSON.parse(body).results;
+  for (let i = 0; i < moviesList.length; i++)
+  {
+    if (moviesList[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/'))
+    {
+      count += 1;
+    }
+  }
+  console.log(count);
 });
