3c7a7dc0083a39a24815d8b6588e11d09ac09bfd
khaledolemy <khaledolemy@gmail.com>
2024-03-27 08:04:22 +0200

    ordered

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,10 +4,10 @@ let reqUrl = 'https://swapi-api.alx-tools.com/api/films/';
 reqUrl = reqUrl + process.argv[2] + '/';
 request(reqUrl, function (n0, n1, body) {
   const charactersList = JSON.parse(body).characters;
-  for (let i = 0; i < charactersList.length; i++) {
-    request(charactersList[i], function (n0, n1, cBody) {
+  charactersList.forEach(element => {
+    request(element, function (n0, n1, cBody) {
       const aName = JSON.parse(cBody).name;
       console.log(aName);
-    });
-  }
+    })
+  });
 });
