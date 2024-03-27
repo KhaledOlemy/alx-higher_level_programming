0c657c3ed5617b254cacbdd05bc27e5db83e0111
khaledolemy <khaledolemy@gmail.com>
2024-03-27 06:57:13 +0200

    style

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,13 @@ const request = require('request');
 const reqUrl = process.argv[2];
 function getSuccess (movie) {
   let tempCount = 0;
-  let tempCharacters = movie.characters;
-  for (let j = 0; j <tempCharacters.length; j++) {
+  const tempCharacters = movie.characters;
+  for (let j = 0; j < tempCharacters.length; j++) {
     if (tempCharacters[j].includes('18')) {
       tempCount += 1;
     }
   }
   return tempCount;
-
 }
 request(reqUrl, function (n0, n1, body) {
   const moviesList = JSON.parse(body).results;
